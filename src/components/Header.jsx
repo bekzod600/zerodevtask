59ab98ceab2578620834285b109335872395e97b
bekzod600 <bekzodbaratov334@gmail.com>
2024-02-21 19:04:45 +0500

    header mobile ended

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,26 @@ import Logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Nav } from "../styles.css";
+import { useState } from "react";
 
 const ListNav = styled.div`
   display: flex;
   gap: 1rem;
   justify-content: center;
   align-items: center;
+  transition: all 0.5s;
+  @media only screen and (max-width: 768px) {
+    position: fixed;
+    z-index: 10;
+    left: 0;
+    right: 0;
+    top: 0;
+    transform: translateY(${(props) => (props.y ? "0%" : "-120%")});
+    padding: 2rem;
+    background-color: white;
+    flex-direction: column;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+  }
 `;
 
 const ListItem = styled.div`
@@ -21,17 +35,43 @@ const NavbarLink = styled(Link)`
   color: #333;
   font-size: 1.2rem;
   font-weight: 600;
-  padding: 0.5rem 1rem;
+  padding: ${(props) => (props.padding == "none" ? 0 : "0.5rem 1rem")};
+`;
+const WelcomeTag = styled.p`
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const MobileMenu = styled.div`
+  cursor: pointer;
+  padding: 0.8rem 1rem;
+  background-color: white;
+  border-radius: 0.3rem;
+  display: none;
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+const CloseButton = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 1rem;
+  display: none;
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
 `;
 
 const Header = () => {
+  const [onMobileMenu, setOnMobileMenu] = useState(0);
   const authUser = useSelector((state) => state.user.authenticatedUser);
   return (
     <Nav>
-      <NavbarLink to="/">
+      <NavbarLink padding="none" to="/">
         <img src={Logo} alt="Logo" className="logo" />
       </NavbarLink>
-      <ListNav>
+      <ListNav y={onMobileMenu}>
         <ListItem>
           <NavbarLink to="/">HOME</NavbarLink>
         </ListItem>
@@ -41,8 +81,14 @@ const Header = () => {
         <ListItem>
           <NavbarLink to="/kredit">KREDIT</NavbarLink>
         </ListItem>
+        <CloseButton onClick={() => setOnMobileMenu(0)}>
+          <i className="fas fa-close"></i>
+        </CloseButton>
       </ListNav>
-      <p className="welcome">Welcome {authUser.user}</p>
+      <WelcomeTag className="welcome">Welcome {authUser.user}</WelcomeTag>
+      <MobileMenu onClick={() => setOnMobileMenu(1)}>
+        <i className="fas fa-bars"></i>
+      </MobileMenu>
     </Nav>
   );
 };
